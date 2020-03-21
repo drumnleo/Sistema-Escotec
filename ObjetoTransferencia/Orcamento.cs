14d1720d40dc40a9057bb4d64fd69943ebb03245
cauhany <cauhany19@gmail.com>
2020-03-21 14:38:35 -0300

    Envio orcamento

--- ObjetoTransferencia/Orcamento.cs
+++ ObjetoTransferencia/Orcamento.cs
@@ -8,8 +8,14 @@ namespace ObjetoTransferencia
 {
    public class Orcamento
     {
+        
         public int Id_Orcamento { get; set; }
         public Pessoa Pessoa { get; set; }
-        public Turma turma1 { get; set; }
+        public Turma Turma1 { get; set; }
+        public Turma Turma2 { get; set; }
+        public Turma Turma3 { get; set; }
+        public Turma Turma4 { get; set; }
+        public PromocaoValor PromocaoValor { get; set; }
+        public DateTime Data_Cadastro { get; set; }
     }
 }
