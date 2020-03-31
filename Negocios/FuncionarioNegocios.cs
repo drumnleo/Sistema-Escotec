eac72963d6257e9c045f85d9d522df2aa2c6ee11
drumnleo <drumnleo@gmail.com>
2020-03-31 16:57:10 -0300

    commit 31032020_1657

--- Negocios/FuncionarioNegocios.cs
+++ Negocios/FuncionarioNegocios.cs
@@ -37,5 +37,47 @@ namespace Negocios
             }
 
         }
+
+        public string AtualizarporId(Funcionario funcionario)
+        {
+            try
+            {
+                acessoDados.LimparParametros();
+                acessoDados.AdicionarParametros("@ID_FUNCIONARIO", funcionario.Id_Funcionario);
+                acessoDados.AdicionarParametros("@ID_PESSOA", funcionario.Id_Pessoa);
+                acessoDados.AdicionarParametros("@DATA_ADMISSAO", funcionario.Data_Admissao);
+                acessoDados.AdicionarParametros("@HORA_ENTRADA", funcionario.Hora_Entrada);
+                acessoDados.AdicionarParametros("@HORA_SAIDA", funcionario.Hora_Saida);
+                acessoDados.AdicionarParametros("@NUM_CTPS", funcionario.Num_CTPS);
+                acessoDados.AdicionarParametros("@SERIE_CTPS", funcionario.Serie_CTPS);
+                acessoDados.AdicionarParametros("@NUMERO_NIS", funcionario.Num_NIS);
+                acessoDados.AdicionarParametros("@ID_USUARIO", funcionario.Usuario_Cad_Alt);
+
+                string idFuncionario = acessoDados.ExecutarManipulacao(CommandType.StoredProcedure, "USP_FUNCIONARIO_ATUALIZARPORID").ToString();
+
+                return idFuncionario;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string Excluir(Funcionario funcionario)
+        {
+            try
+            {
+                acessoDados.LimparParametros();
+                acessoDados.AdicionarParametros("ID_FUNCIONARIO", funcionario.Id_Funcionario);
+                acessoDados.AdicionarParametros("ID_USUARIO", funcionario.Usuario_Cad_Alt);
+                string idFuncionario = acessoDados.ExecutarManipulacao(CommandType.StoredProcedure, "USP_FUNCIONARIO_EXCLUIR").ToString();
+                return idFuncionario;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
     }
 }
