a2829a39261a5ac519abb9feaa06026f77452f60
drumnleo <drumnleo@gmail.com>
2020-04-01 23:08:45 -0300

    commit

--- Apresentacao/Presentation/Popup/SearchDialogs/SearchDialog_Funcionario .cs
+++ Apresentacao/Presentation/Popup/SearchDialogs/SearchDialog_Funcionario .cs
@@ -29,14 +29,7 @@ namespace Apresentacao.Presentation.Popup.SearchDialogs
 
         private void SearchDialog_Load(object sender, EventArgs e)
         {
-            CarregaComboBox();
-        }
-
-        private void CarregaComboBox()
-        {
-
-            FuncionarioNegocios funcionarioNegocios = new FuncionarioNegocios();
-
+            
         }
 
         private void SearchDialog_Shown(object sender, EventArgs e)
@@ -81,8 +74,6 @@ namespace Apresentacao.Presentation.Popup.SearchDialogs
 
         private void btnpesquisar_Click(object sender, EventArgs e)
         {
-            ClearTextBoxes(this.Controls);
-            Enableallcontrols();
             CarregaComboBox();
             PessoaNegocios pessoaNegocios = new PessoaNegocios();
             PessoaColecao pessoaColecao = pessoaNegocios.ConsultarPorDescricao(tbxSearch.Text);
@@ -91,107 +82,6 @@ namespace Apresentacao.Presentation.Popup.SearchDialogs
             dataGrid.DataSource = pessoaColecao;
             dataGrid.Update();
             dataGrid.Refresh();
-
-
-        }
-
-        private void btnNovo_Click(object sender, EventArgs e)
-        {
-            if (btnNovo.Text == "Novo")
-            {
-                dataGrid.DataSource = null;
-                dataGrid.Refresh();
-                CarregaComboBox();
-                ClearTextBoxes(this.Controls);
-                Enableallcontrols();
-                btnNovo.Text = "Salvar";
-                btnAtualizar.Visible = false;
-                btnSelecionar.Visible = false;
-            }
-            else if (btnNovo.Text == "Salvar")
-            {
-                Pessoa pessoa = new Pessoa();
-                PessoaNegocios pessoaNegocios = new PessoaNegocios();
-                Validadocs validadocs = new Validadocs();
-
-                pessoa.Id_Profissao = Convert.ToInt32(cbxProfissao.SelectedValue);
-                pessoa.Id_TipoDoc = Convert.ToInt32(cbxTipoDoc.SelectedValue);
-                pessoa.Id_EstadoCivil = Convert.ToInt32(cbxEstadoCivil.SelectedValue);
-                pessoa.Nome = tbxNome.Text;
-                pessoa.Sobrenome = tbxSobrenome.Text;
-                pessoa.CPF = validadocs.SemFormatacao(tbxCPF.Text);
-                pessoa.Doc = tbxNumDoc.Text;
-                pessoa.Data_Nasc = dtnascimento.Value;
-                pessoa.Email = tbxEmail.Text;
-                pessoa.Pai = tbxPai.Text;
-                pessoa.Mae = tbxMae.Text;
-                if (ckboxfem.Checked == true)
-                {
-                    pessoa.Sexo = 'F';
-                }
-                else if (ckboxmasc.Checked == true)
-                {
-                    pessoa.Sexo = 'M';
-                }
-                else
-                {
-                    MessageBox.Show("Há um erro na seleção do sexo, verifique!");
-                }
-                pessoa.Id_Usuario = LoginNegocios.UsuarioLogadoGetSet.Id_Usuario;
-
-                string retorno = pessoaNegocios.Inserir(pessoa);
-
-                try
-                {
-                    int idPessoa = Convert.ToInt32(retorno);
-                    Pessoa pessoaCadastrada = pessoaNegocios.ConsultarPorId(idPessoa);
-                    MessageBox.Show("Cadastro base inserido com sucesso!");
-                    AdicionarEditarFuncionario.PessoaGetSet = pessoaCadastrada;
-                    this.Close();
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Erro ao cadastrar. Detalhes: " + retorno);
-                }
-            }
-        }
-
-        private void ClearTextBoxes(Control.ControlCollection ctrlCollection)
-        {
-            foreach (Control ctrl in ctrlCollection)
-            {
-                if (ctrl is TextBoxBase)
-                {
-                    ctrl.Text = String.Empty;
-                    ctrl.Enabled = true;
-                }
-                else
-                {
-                    ClearTextBoxes(ctrl.Controls);
-                }
-            }
-
-            ckboxfem.Enabled = true;
-            ckboxmasc.Enabled = true;
-            dtnascimento.Enabled = true;
-        }
-
-        private void Enableallcontrols()
-        {
-            tbxNome.Enabled = true;
-            tbxSobrenome.Enabled = true;
-            tbxCPF.Enabled = true;
-            tbxEmail.Enabled = true;
-            tbxMae.Enabled = true;
-            tbxPai.Enabled = true;
-            tbxNumDoc.Enabled = true;
-            tbxSearch.Enabled = true;
-            cbxEstadoCivil.Enabled = true;
-            cbxProfissao.Enabled = true;
-            cbxTipoDoc.Enabled = true;
-            ckboxfem.Enabled = true;
-            ckboxmasc.Enabled = true;
-            dtnascimento.Enabled = true;
         }
 
         private void btnSair_Click(object sender, EventArgs e)
@@ -199,126 +89,6 @@ namespace Apresentacao.Presentation.Popup.SearchDialogs
             Pessoa pessoa = new Pessoa();
             AdicionarEditarFuncionario.PessoaGetSet = pessoa;
             this.Close();
-        }
-
-        public static string FormatCNPJ(string CNPJ)
-        {
-            return Convert.ToUInt64(CNPJ).ToString(@"00\.000\.000\/0000\-00");
-        }
-
-        private void tbxCPF_Leave(object sender, EventArgs e)
-        {
-            Validadocs validadocs = new Validadocs();
-            tbxCPF.Text = validadocs.SemFormatacao(tbxCPF.Text);
-
-            if (tbxCPF.Text.Length != 11)
-            {
-                tbxCPF.Text = "";
-            }
-            if (tbxCPF.Text.Length == 0)
-            {
-
-            }
-            else
-            {
-                tbxCPF.Text = validadocs.FormatCPF(tbxCPF.Text);
-                bool validacpf = validadocs.ValidaCPF(tbxCPF.Text);
-                if (validacpf)
-                {
-                    lblavisocpf.Text = "CPF Válido";
-                }
-                else
-                {
-                    MessageBox.Show("CPF inválido!");
-                    lblavisocpf.Text = "(somente números sem pontos)";
-                    tbxCPF.Text = "";
-                }
-            }
-        }
-
-        private void tbxCPF_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsDigit(e.KeyChar))
-            {
-                e.Handled = true;
-                MessageBox.Show("este campo aceita somente numero");
-            }
-        }
-
-        private void ckboxmasc_OnChange(object sender, EventArgs e)
-        {
-            if (ckboxmasc.Checked == true)
-            {
-                ckboxfem.Checked = false;
-            }
-        }
-
-        private void ckboxfem_OnChange(object sender, EventArgs e)
-        {
-            if (ckboxfem.Checked == true)
-            {
-                ckboxmasc.Checked = false;
-            }
-        }
-
-        private void btnAtualizar_Click(object sender, EventArgs e)
-        {
-            if (btnAtualizar.Text == "Atualizar")
-            {
-                Enableallcontrols();
-                btnAtualizar.Text = "Gravar";
-                btnNovo.Visible = false;
-                btnSelecionar.Visible = false;
-                btnAtualizar.Location = new Point(410, 291);
-            }
-            else if (btnAtualizar.Text == "Gravar")
-            {
-
-                Pessoa pessoa = new Pessoa();
-                PessoaNegocios pessoaNegocios = new PessoaNegocios();
-                Validadocs validadocs = new Validadocs();
-
-                pessoa.Id_Profissao = Convert.ToInt32(cbxProfissao.SelectedValue);
-                pessoa.Id_TipoDoc = Convert.ToInt32(cbxTipoDoc.SelectedValue);
-                pessoa.Id_EstadoCivil = Convert.ToInt32(cbxEstadoCivil.SelectedValue);
-                pessoa.Nome = tbxNome.Text;
-                pessoa.Sobrenome = tbxSobrenome.Text;
-                pessoa.CPF = validadocs.SemFormatacao(tbxCPF.Text);
-                pessoa.Doc = tbxNumDoc.Text;
-                pessoa.Data_Nasc = dtnascimento.Value;
-                pessoa.Email = tbxEmail.Text;
-                pessoa.Pai = tbxPai.Text;
-                pessoa.Mae = tbxMae.Text;
-                if (ckboxfem.Checked == true)
-                {
-                    pessoa.Sexo = 'F';
-                }
-                else if (ckboxmasc.Checked == true)
-                {
-                    pessoa.Sexo = 'M';
-                }
-                else
-                {
-                    MessageBox.Show("Há um erro na seleção do sexo, verifique!");
-                }
-                pessoa.Id_Usuario = LoginNegocios.UsuarioLogadoGetSet.Id_Usuario;
-
-                string retorno = pessoaNegocios.AtualizarporId(pessoa);
-
-                try
-                {
-                    int idPessoa = Convert.ToInt32(retorno);
-                    Pessoa pessoaCadastrada = pessoaNegocios.ConsultarPorId(idPessoa);
-                    MessageBox.Show("Cadastro base atualizado com sucesso!");
-                    AdicionarEditarFuncionario.PessoaGetSet = pessoaCadastrada;
-                    this.Close();
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Erro ao atualizar. Detalhes: " + retorno);
-                }
-            }
-
         }
 
         private void btnSelecionar_Click(object sender, EventArgs e)
